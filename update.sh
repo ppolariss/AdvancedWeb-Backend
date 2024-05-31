88ba0bf18dd1fd58240edb20c1d943cf2a815e4a
polaris <2673986789@qq.com>
2024-05-31 16:24:24 +0800

    docker compose

--- update.sh
+++ update.sh
@@ -2,3 +2,7 @@ docker compose -p awb pull
 docker compose -p awb down
 docker volume rm awb_shared_volume
 docker compose -p awb up -d
+# cd Desktop/AdvancedWeb-Backend/
+# docker compose -p awb pull
+# docker volume rm awb_shared_volume
+# docker compose -p awb up -d --build
